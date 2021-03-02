c2dd2391574d60a2339a8081e94185fc6efc8f54
Andrea <aagiollo@gmail.com>
2021-03-02 12:28:16 +0100

    IDS runner bat

--- detonar/run_ids.bat
+++ detonar/run_ids.bat
@@ -13,15 +13,11 @@ if "%condition%" == "true" (
 	python feature_extractor.py --scenario=%SCENARIO% --simulation_time=%SIM_TIME% --time_window=%WINDOW% --lag_val=%LAG%
 )
 python features_plots.py --scenario=%SCENARIO% --simulation_time=%SIM_TIME% --time_window=%WINDOW% --lag_val=%LAG%
-::echo Extracting Features...
-::set sims=simulation-101,simulation-102,simulation-103,simulation-104,simulation-105
-::set sims=simulation-111,simulation-112,simulation-113,simulation-114,simulation-115
-::set sims=simulation-201,simulation-202,simulation-203,simulation-204,simulation-205
-set sims=simulation-104
-::echo list="%sims%"
+echo Extracting Features...
+set sims=simulation-101,simulation-102,simulation-103,simulation-104,simulation-105
 for %%a in ("%sims:,=" "%") do (
     echo %%a
 	@echo on
-	::python new_arima_ids.py --scenario=%SCENARIO% --chosen_simulation=%%a --simulation_time=%SIM_TIME% --time_window=%WINDOW% --lag_val=%LAG%
+	python new_arima_ids.py --scenario=%SCENARIO% --chosen_simulation=%%a --simulation_time=%SIM_TIME% --time_window=%WINDOW% --lag_val=%LAG%
 	@echo off
 )
