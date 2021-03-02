ec0f0a7d52862e258c6a77af8cc08b6d028613e4
Andrea <aagiollo@gmail.com>
2021-03-02 15:57:50 +0100

    IDS runner bat

--- detonar/run_ids_over_all_attacks.bat
+++ detonar/run_ids_over_all_attacks.bat
@@ -1,11 +1,9 @@
 @echo off
 :: set variables
 set SCENARIOS=Blackhole,Clone_ID,Continuous_Sinkhole,DIS,Hello_Flood,Legitimate,Local_Repair,Rank,Replay,Selective_Forward,Sinkhole,Sybil,Version,Wormhole,Worst_Parent
-::set SCENARIOS=Worst_Parent
-::set SIMULATIONS=simulation-101,simulation-102,simulation-103,simulation-104,simulation-105
-::set SCENARIOS=Legitimate
+set SIMULATIONS=simulation-101,simulation-102,simulation-103,simulation-104,simulation-105
 ::set SIMULATIONS=simulation-111,simulation-112,simulation-113,simulation-114,simulation-115
-set SIMULATIONS=simulation-201,simulation-202,simulation-203,simulation-204,simulation-205
+::set SIMULATIONS=simulation-201,simulation-202,simulation-203,simulation-204,simulation-205
 :: Start with shell commands
 :: Activate python virtual environment and run python script
 CALL ..\ids-env\Scripts\activate.bat 
