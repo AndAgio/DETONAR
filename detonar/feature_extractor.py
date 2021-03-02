d62bc14b77e929ff66e81d67439eb4f7353773dc
Andrea <aagiollo@gmail.com>
2021-03-02 12:33:46 +0100

    Feature extractor

--- detonar/feature_extractor.py
+++ detonar/feature_extractor.py
@@ -50,45 +50,36 @@ def get_time_window_data(data, index, args, full_data=False):
         start_time = index * time_window + args.time_start
     end_time = (index + 1) * time_window + args.time_start
     # Get all packets that have been received at the network layer between start and end time (depending on window size)
-    # condition = (data['NW_LAYER_ARRIVAL_TIME(US)']>start_time) & (data['NW_LAYER_ARRIVAL_TIME(US)']<=end_time)
     condition = (data[args.time_feat_micro] > start_time) & (data[args.time_feat_micro] <= end_time)
     sequence = data[condition]
-    # print(sequence)
     return sequence
 
 
 def get_features(data, node_name, args):  # full_data, node_name, args):
-    # print("Node name: {}".format(node_name))
     features = np.zeros((args.n_features), dtype=float)
     # Get received and sent packets
     received_packets = data[data['RECEIVER_ID'] == node_name]
     transmitted_packets = data[data['TRANSMITTER_ID'] == node_name]
-    # print(received_packets)
     # Get number of DIO received
     received_count = received_packets['CONTROL_PACKET_TYPE/APP_NAME'].value_counts()
-    # print('\nReceived count:\n{}'.format(received_count))
     if ('DIO' in received_count):
         features[0] = received_count['DIO']
     # Get number of DIO transmitted
     transmitted_count = transmitted_packets['CONTROL_PACKET_TYPE/APP_NAME'].value_counts()
-    # print('\nTransmitted count:\n{}'.format(transmitted_count))
     if ('DIO' in transmitted_count):
         features[1] = transmitted_count['DIO']
-
     # Get number of DAO received
     if ('DAO' in received_count):
         features[2] = received_count['DAO']
     # Get number of DAO transmitted
     if ('DAO' in transmitted_count):
         features[3] = transmitted_count['DAO']
-
     # Get number of DIS received
     if ('DIS' in received_count):
         features[4] = received_count['DIS']
     # Get number of DAO transmitted
     if ('DIS' in transmitted_count):
         features[5] = transmitted_count['DIS']
-
     # Get number of application packets received
     received_app_count = received_packets['PACKET_TYPE'].value_counts()
     if ('Control_Packet' in received_app_count):
@@ -101,7 +92,6 @@ def get_features(data, node_name, args):  # full_data, node_name, args):
         features[7] = transmitted_app_count.sum() - transmitted_app_count['Control_Packet']
     else:
         features[7] = transmitted_app_count.sum()
-
     # Get all packets received/transmitted by the node
     all_packets = data[(data['RECEIVER_ID'] == node_name) | (data['TRANSMITTER_ID'] == node_name)]
     app_list = all_packets['CONTROL_PACKET_TYPE/APP_NAME'].value_counts().index.to_list()
@@ -111,19 +101,15 @@ def get_features(data, node_name, args):  # full_data, node_name, args):
         if (control in app_list):
             app_list.remove(control)
     features[8] = len(app_list)
-
     # Get number of different source IPs
     source_ips = all_packets['SOURCE_IP'].value_counts().index.to_list()
     features[9] = len(source_ips)
-
     # Get number of different source IPs
     destination_ips = all_packets['DESTINATION_IP'].value_counts().index.to_list()
     features[10] = len(destination_ips)
-
     # Get number of Gateway IPs
     gateway_ips = transmitted_packets['GATEWAY_IP'].value_counts().index.to_list()
     features[11] = len(gateway_ips)
-
     # Get successful transmission rate
     successful = transmitted_packets[transmitted_packets['PACKET_STATUS'] == 'Successful']
     collided = transmitted_packets[transmitted_packets['PACKET_STATUS'] == 'Collided']
@@ -132,49 +118,24 @@ def get_features(data, node_name, args):  # full_data, node_name, args):
     else:
         rate = len(successful.index) / len(transmitted_packets.index) * 100
     features[12] = rate
-
     # Get number of broadcast packets sent
     broadcast = transmitted_packets[transmitted_packets['DESTINATION_IP'] == 'FF00:0:0:0:0:0:0:0']
     features[13] = len(broadcast.index)
-
-    '''
-    #Get neighbours (number of different nodes who received DIO messages sent by this node)
-    transmitted_dios = transmitted_packets[transmitted_packets['CONTROL_PACKET_TYPE/APP_NAME'] == 'DIO']
-    neighbours = transmitted_dios['RECEIVER_ID'].value_counts()
-    features[14] = len(neighbours.index)
-
-    #Get number of nexthop IP
-    all_transmitted_packets = full_data[full_data['TRANSMITTER_ID'] == node_name]
-    next_hop_ips = all_transmitted_packets[all_transmitted_packets['NEXT_HOP_IP'] != 'FF00:0:0:0:0:0:0:0']['NEXT_HOP_IP'].value_counts().index.to_list()
-    features[15] = len(next_hop_ips)
-    #print('next_hop_ips: {}'.format(len(next_hop_ips)))
-    '''
-
     # Get number of incoming application packets that do not have itself as destination
     received_app_pcks = received_packets[received_packets['PACKET_TYPE'] == 'Sensing']
-    # print('received_app_pcks: {}'.format(received_app_pcks))
     incoming = received_app_pcks[(received_app_pcks['DESTINATION_ID'] != received_app_pcks['RECEIVER_ID']) & (received_app_pcks['PACKET_STATUS'] == 'Successful')]
     n_incoming_app_pcks = len(incoming.index)
-    # print('n_incoming_app_pcks: {}'.format(n_incoming_app_pcks))
     # Get number of outgoing application packets that do not have itself as source
     transmitted_app_pcks = transmitted_packets[transmitted_packets['PACKET_TYPE'] == 'Sensing']
-    # print('received_app_pcks: {}'.format(received_app_pcks))
     outgoing = transmitted_app_pcks[
         transmitted_app_pcks['SOURCE_ID'] != transmitted_app_pcks['TRANSMITTER_ID']]
     n_outgoing_app_pcks = len(outgoing.index)
-    # print('n_outgoing_app_pcks: {}'.format(n_outgoing_app_pcks))
     if (n_incoming_app_pcks == n_outgoing_app_pcks):
-        features[14] = 1#00
+        features[14] = 1
     elif (n_outgoing_app_pcks != 0 and n_incoming_app_pcks == 0):
-        features[14] = 1#00
+        features[14] = 1
     else:
         features[14] = n_outgoing_app_pcks / n_incoming_app_pcks #* 100
-
-    # Get number of nodes transmitting packets in this time window
-    # list_nodes = data['TRANSMITTER_ID'].value_counts().index.to_list()
-    # list_nodes = [node for node in list_nodes if 'SENSOR' in node]
-    # print('list_nodes: {}'.format(list_nodes))
-    # features[17] = list_nodes
 
     return features
 
@@ -186,7 +147,6 @@ def main():
     filenames = glob.glob(os.path.join(os.getcwd(), '..', args.data_dir, args.scenario,
                                        'Packet_Trace_' + str(int(args.simulation_time)) + 's', '*.csv'))
     filenames.sort()
-    # print(filenames)
     ###################################################################################
     # This part is for debug only while trying out with parallelization
     if (args.single_sim != ''):
@@ -214,7 +174,6 @@ def main():
             # For each index get the corresponding network traffic window and extract the features in that window
             for index in range(start_index, end_index):
                 time_sequence = get_time_window_data(data, index, args, full_data=False)
-                # time_sequence_from_0_to_index = get_time_window_data(data, index, args, full_data=True)
                 features = get_features(time_sequence, nodes_names[node_index],
                                         args)  # time_sequence_from_0_to_index, nodes_names[node_index], args)
                 # Append the features row to the matrix that will contain the series of all features
@@ -288,7 +247,6 @@ def parallelized_main():
         for index in range(start_index, end_index):
             time_sequence = get_time_window_data(data, index, args, full_data=False)
             windows.append(time_sequence)
-        # print('Windows length: {}'.format(len(windows)))
         # For each node in the simulation take all time windows and extract the features
         for node_index in range(len(nodes_names)):
             matrix = []
@@ -298,13 +256,6 @@ def parallelized_main():
                                              itertools.repeat(nodes_names[node_index], len(windows)),
                                              itertools.repeat(args, len(windows))):
                     matrix.append(features)
-                    # print(matrix)
-            '''
-            for index in range(len(windows)):
-                features = get_features(windows[index], nodes_names[node_index], args)
-                #Append the features row to the matrix that will contain the series of all features
-                matrix.append(features)
-            '''
             matrix = np.asarray(matrix)
             # If the cumulative features are considered then we need to compute the cumulative sum for each feature through time
             if (args.cumulative_sum == 'True'):
@@ -328,12 +279,8 @@ def parallelized_main():
             pandas_matrix.to_csv(name, index=False, header=True)
 
         # Storing file with DAOs only
-        # print('Storing DAOs csv')
         dao_packets = data[data['CONTROL_PACKET_TYPE/APP_NAME'] == 'DAO']
-        #dao_packets = dao_packets[['SOURCE_ID', 'DESTINATION_ID', 'NW_LAYER_ARRIVAL_TIME(US)']]
         dao_packets = dao_packets[['SOURCE_ID', 'DESTINATION_ID', args.time_feat_micro]]
-        # print(dao_packets['NW_LAYER_ARRIVAL_TIME(US)'])
-        #dao_packets = dao_packets.rename(columns={'NW_LAYER_ARRIVAL_TIME(US)': 'NW_LAYER_ARRIVAL_TIME(S)'})
         dao_packets = dao_packets.rename(columns={args.time_feat_micro: args.time_feat_sec})
         name = args.out_feat_files + '/' + args.scenario + '/simulation-' + \
                filenames[file_index].split("/")[-1].split("\\")[-1].split(".")[0] + \
@@ -344,10 +291,7 @@ def parallelized_main():
         dao_packets.to_csv(name, index=False, header=True)
 
         # Storing file with DIOs only
-        # print('Storing DIOs csv')
         dio_packets = data[data['CONTROL_PACKET_TYPE/APP_NAME'] == 'DIO']
-        #dio_packets = dio_packets[['TRANSMITTER_ID', 'RECEIVER_ID', 'NW_LAYER_ARRIVAL_TIME(US)']]
-        #dio_packets = dio_packets.rename(columns={'NW_LAYER_ARRIVAL_TIME(US)': 'NW_LAYER_ARRIVAL_TIME(S)'})
         dio_packets = dio_packets[['TRANSMITTER_ID', 'RECEIVER_ID', args.time_feat_micro]]
         dio_packets = dio_packets.rename(columns={args.time_feat_micro: args.time_feat_sec})
         name = args.out_feat_files + '/' + args.scenario + '/simulation-' + \
@@ -359,10 +303,7 @@ def parallelized_main():
         dio_packets.to_csv(name, index=False, header=True)
 
         # Storing file with Sensing packets only
-        # print('Storing APPs csv')
         app_packets = data[data['PACKET_TYPE'] == 'Sensing']
-        #app_packets = app_packets[['TRANSMITTER_ID', 'RECEIVER_ID', 'NW_LAYER_ARRIVAL_TIME(US)']]
-        #app_packets = app_packets.rename(columns={'NW_LAYER_ARRIVAL_TIME(US)': 'NW_LAYER_ARRIVAL_TIME(S)'})
         app_packets = app_packets[['TRANSMITTER_ID', 'RECEIVER_ID', args.time_feat_micro]]
         app_packets = app_packets.rename(columns={args.time_feat_micro: args.time_feat_sec})
         name = args.out_feat_files + '/' + args.scenario + '/simulation-' + \
@@ -374,9 +315,6 @@ def parallelized_main():
         app_packets.to_csv(name, index=False, header=True)
 
         # Storing file with any packet but transmitter and time only
-        # print('Storing tx_time csv')
-        #all_packets = data[['TRANSMITTER_ID', 'NW_LAYER_ARRIVAL_TIME(US)', 'NEXT_HOP_IP']]
-        #all_packets = all_packets.rename(columns={'NW_LAYER_ARRIVAL_TIME(US)': 'NW_LAYER_ARRIVAL_TIME(S)'})
         all_packets = data[['TRANSMITTER_ID', args.time_feat_micro, 'NEXT_HOP_IP']]
         all_packets = all_packets.rename(columns={args.time_feat_micro: args.time_feat_sec})
         name = args.out_feat_files + '/' + args.scenario + '/simulation-' + \
@@ -388,10 +326,7 @@ def parallelized_main():
         all_packets.to_csv(name, index=False, header=True)
 
         # Storing file with control packets and ranks & versions
-        # print('Storing RANKs csv')
         ranks_vers = data[data['PACKET_TYPE'] == 'Control_Packet']
-        #ranks_vers = ranks_vers[['TRANSMITTER_ID', 'CONTROL_PACKET_TYPE/APP_NAME', 'NW_LAYER_ARRIVAL_TIME(US)', 'RPL_RANK', 'RPL_VERSION']]
-        #ranks_vers = ranks_vers.rename(columns={'NW_LAYER_ARRIVAL_TIME(US)': 'NW_LAYER_ARRIVAL_TIME(S)'})
         ranks_vers = ranks_vers[['TRANSMITTER_ID', 'CONTROL_PACKET_TYPE/APP_NAME', args.time_feat_micro, 'RPL_RANK', 'RPL_VERSION']]
         ranks_vers = ranks_vers.rename(columns={args.time_feat_micro: args.time_feat_sec})
 
@@ -404,9 +339,11 @@ def parallelized_main():
         ranks_vers.to_csv(name, index=False, header=True)
 
     toc = time.time()
-    # print('Time taken with parallelization: {}'.format(toc-tic))
 
 
 if __name__ == '__main__':
-    # main()
-    parallelized_main()
+    parallelization = True
+    if parallelization:
+        parallelized_main()
+    else:
+        main()
